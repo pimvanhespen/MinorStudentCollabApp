c2fcbbd08dfc873f4fafd86eae932bd66de3e3ea
Danny Cao <danny.dc.cao@gmail.com>
2020-09-03 13:59:36 +0200

    buttons linked

---
+++ scripts/buttons.js
@@ -0,0 +1,11 @@
+$("#button-begonnen").click(function() {
+    $("#test-button").append("<p>Begonnen</p>");
+});
+
+$("#button-afgerond").click(function() {
+    $("#test-button").append("<p>Afgerond</p>");
+});
+
+$("#button-help").click(function() {
+    $("#test-button").append("<p>Help</p>");
+});
